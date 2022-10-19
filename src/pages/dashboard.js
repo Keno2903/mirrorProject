6c8a1749ea99c033634cd74ecfd0cf4d89a488fd
Keno2903 <Keno.goellner@gmail.com>
2022-10-19 19:24:44 +0200

    grid

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from "react";
+import { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function Dashboard() {
+  const layout = [
+    { i: "a", x: 0, y: 0, w: 1, h: 1, isResizable: false },
+    { i: "b", x: 1, y: 0, w: 1, h: 1, isResizable: false },
+    { i: "c", x: 2, y: 0, w: 1, h: 1, isResizable: false },
+    { i: "d", x: 0, y: 1, w: 1, h: 1, isResizable: false },
+    { i: "e", x: 1, y: 1, w: 1, h: 1, isResizable: false },
+    { i: "f", x: 2, y: 1, w: 1, h: 1, isResizable: false },
+    { i: "g", x: 0, y: 2, w: 1, h: 1, isResizable: false },
+    { i: "h", x: 1, y: 2, w: 1, h: 1, isResizable: false },
+    { i: "i", x: 2, y: 2, w: 1, h: 1, isResizable: false },
+  ];
+
   return (
-    <div>dashboard</div>
-  )
+    <ResponsiveGridLayout
+      layouts={{ lg: layout }}
+      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      cols={{ lg: 3, md: 3, sm: 3, xs: 2, xxs: 1 }}
+      rowHeight={300}
+      width={1200}
+    >
+      <div key="a" style={{ background: "grey" }}>
+        a
+      </div>
+      <div key="b" style={{ background: "grey" }}>
+        b
+      </div>
+      <div key="c" style={{ background: "grey" }}>
+        c
+      </div>
+      <div key="d" style={{ background: "grey" }}>
+        d
+      </div>
+      <div key="e" style={{ background: "grey" }}>
+        e
+      </div>
+      <div key="f" style={{ background: "grey" }}>
+        f
+      </div>
+      <div key="g" style={{ background: "grey" }}>
+        g
+      </div>
+      <div key="h" style={{ background: "grey" }}>
+        h
+      </div>
+      <div key="i" style={{ background: "grey" }}>
+        i
+      </div>
+    </ResponsiveGridLayout>
+  );
 }
