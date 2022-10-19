3e12aa3cb031497dd2c9ae7ab362af4e42684d07
Keno2903 <Keno.goellner@gmail.com>
2022-10-19 19:33:47 +0200

    added solid 3x3 grid

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
 import axios from "axios";
 const ResponsiveGridLayout = WidthProvider(Responsive);
@@ -12,7 +12,7 @@ const getBackend = () => {
         console.log(response);
     })
 }
-  const layout = [
+  const [layout, setLayout] = useState([
     { i: "a", x: 0, y: 0, w: 1, h: 1, isResizable: false },
     { i: "b", x: 1, y: 0, w: 1, h: 1, isResizable: false },
     { i: "c", x: 2, y: 0, w: 1, h: 1, isResizable: false },
@@ -22,7 +22,7 @@ const getBackend = () => {
     { i: "g", x: 0, y: 2, w: 1, h: 1, isResizable: false },
     { i: "h", x: 1, y: 2, w: 1, h: 1, isResizable: false },
     { i: "i", x: 2, y: 2, w: 1, h: 1, isResizable: false },
-  ];
+  ]);
 
   return (
     <ResponsiveGridLayout
@@ -30,8 +30,9 @@ const getBackend = () => {
       breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
       cols={{ lg: 3, md: 3, sm: 3, xs: 2, xxs: 1 }}
       rowHeight={300}
-      onLayoutChange={getBackend}
+      onLayoutChange={e=>setLayout(fixLayout(e))}
       width={1200}
+      maxRows={3}
     >
       <div key="a" style={{ background: "grey" }}>
         a
@@ -62,4 +63,37 @@ const getBackend = () => {
       </div>
     </ResponsiveGridLayout>
   );
+}
+
+const fixLayout = (layout) => {
+  // `y` is calculated by `h` in the layout object, since `h` is 20
+  // first row will be 0, second 20, third 40
+  const maxY = 2
+
+  // when an item goes to a new row, there is an empty column in the maxY row
+  // so here we find which columns exist
+  // tslint:disable-next-line:max-line-length
+  const maxRowXs = layout.map((item) => item.y === maxY ? item.x : null).filter((value) => value !== null)
+
+  // xs or cols, we only have 3 cols
+  const xs = [0,1,2]
+
+  // find the missing col
+  // tslint:disable-next-line:max-line-length
+  const missingX = xs.find((value) => maxRowXs.every((maxRowX) => maxRowX !== value))
+
+  // bring the item from the new row into maxY row
+  // and place it in the missing column
+  const fixedLayout = layout.map((item) => {
+    if (item.y > maxY) {
+      const fixedItem = {
+        ...item,
+        y: maxY,
+        x: missingX
+      }
+      return fixedItem
+    }
+    return item
+  })
+  return fixedLayout
 }
